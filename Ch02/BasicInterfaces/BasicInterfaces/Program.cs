1b659ce18369843de3b8c7e532b30045b77c774e
Neous1 <Neous1@users.noreply.github.com>
2017-12-11 12:00:13 -0500

    setup

---
+++ Ch02/BasicInterfaces/BasicInterfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInterfaces
+{
+    interface IStorable
+    {
+        void Save();
+        void Load();
+        Boolean NeedsSave { get; set; }
+    }
+    class Document
+    {
+        private string name;
+
+        public Document(string s)
+        {
+            name = s;
+            Console.WriteLine("Created a document with name '{0}'", s);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            Document d = new Document("Test Document");
+
+
+            Console.WriteLine("\nPress Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
