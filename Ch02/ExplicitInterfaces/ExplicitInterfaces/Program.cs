da610e09d7afe16cd3124f8d865be94233faaf92
Neous1 <Neous1@users.noreply.github.com>
2017-12-11 12:53:27 -0500

    add interfaces IFoo and IBar and a class FooBar

---
+++ Ch02/ExplicitInterfaces/ExplicitInterfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterfaces
+{
+    interface IFoo
+    {
+        void SomeMethod();
+    }
+    interface IBar
+    {
+        void SomeMethod();
+    }
+
+    class FooBar : IFoo, IBar
+    {
+        public void SomeMethod()
+        {
+            Console.WriteLine("This is the class SomeMethod");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args) {
+            
+            Console.ReadLine();
+        }
+    }
+}
